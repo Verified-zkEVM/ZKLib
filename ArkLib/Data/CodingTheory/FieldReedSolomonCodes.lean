02b0bb538921ecb38e2bce712359daba293fe803
PlanetMacro <mirco@leastauthority.com>
2025-05-13 18:23:09 +0200

    FieldRSC, SmoothDom, MockListdecode

--- ArkLib/Data/CodingTheory/FieldReedSolomonCodes.lean
+++ ArkLib/Data/CodingTheory/FieldReedSolomonCodes.lean
@@ -4,36 +4,40 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Least Authority
 -/
 import ArkLib.Data.CodingTheory.ReedSolomon
+import ArkLib.Data.CodingTheory.RelativeHammingDistance
 
 section FieldRSC
 
 open Polynomial Finset ReedSolomon LinearMap
 
-variable {F : Type*} [Field F] {n : ℕ} {domain : Fin n ↪ F} {deg : ℕ}
+variable {F : Type*} [Field F]
+         {ι : Type*}  [Fintype ι] [DecidableEq ι] -- actually ι is the domain
+         {domain : ι ↪ F}  -- domain is the set of word, where codes are a subset
+         {deg : ℕ}
 
-/--  The linear map that maps F-valued vectors ( c(x₀),...,c(xₙ₋₁) ) to degree < n polynomials p,
-    such that p(xᵢ) = c(xᵢ) for all i ∈ {0,..,n-1} -/
-noncomputable def interpolate : (Fin n → F) →ₗ[F] F[X] :=
+/-- The linear map that maps functions `f: ι→ F` to degree < n polynomials p,
+    such that p(x) = f(x) for all x ∈ ι -/
+noncomputable def interpolate: (ι→ F) →ₗ[F] F[X] :=
   Lagrange.interpolate univ domain
 
 /-- The linear map that maps Reed Solomon Code words to their associated
    < deg degree polynomials -/
-noncomputable def decode: (code domain deg) →ₗ[F] F[X] :=
-    domRestrict (interpolate (domain := domain)) (code domain deg)
+noncomputable def decode: (code F ι domain deg) →ₗ[F] F[X] :=
+    domRestrict (interpolate (domain := domain)) (code F ι domain deg)
 
 /-- the decoded degree < deg polynomial of a Reed Solomon code `c` -/
 noncomputable def decoded
-  (c : code domain deg) : F[X] :=
+  (c : code F ι domain deg) : F[X] :=
     decode.toFun c
 
--- Should be in SemiRing RSC
--- Could also use just deg / n
-noncomputable def rate (_C : code domain deg) : ℝ := deg / (Fintype.card (Fin n) : ℝ)
+-- Should be in LinearCodes.lean
+noncomputable def rate (_C : code F ι domain deg) : ℝ := deg / (Fintype.card ι)
 
--- Could also be in SemiRingRSC or in outOfDomainSampl.
-/-- Complement of the domain in a finite field `F` i.e. `F\domain` as a Finset -/
-def domainComplement [Fintype F] [DecidableEq F] (_C : code domain deg) : Finset F :=
-  let domain_finset : Finset F := (univ : Finset (Fin n)).image domain
-  Finset.univ \ domain_finset
+
+-- TODO: This should be in ReedSolomon.lean
+-- Nethermind provided conflicting definitions for LinarCodes
+-- This is for the one in ArkLib.Data.CodingTheory.RelativeHammingDistance
+def toLinearCode : LinearCode ι F :=
+  code F ι domain deg
 
 end FieldRSC
