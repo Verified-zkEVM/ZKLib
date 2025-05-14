c00f82bd0cc18cf7ad0080bcde8eeb79d3ee4f9b
dpoulami <poulami.kgp.cse@gmail.com>
2025-05-14 16:12:28 +0200

    updated Combine with new RSC defs

--- ArkLib/ProofSystem/Stir/Combine.lean
+++ ArkLib/ProofSystem/Stir/Combine.lean
@@ -4,137 +4,105 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Least Authority
 -/
 
-import ArkLib.ProofSystem.Stir.ToCodingTheory.ProximityBound
-import ArkLib.ProofSystem.Stir.ToCodingTheory.FracHammingDist
-import ArkLib.ProofSystem.Stir.ToCodingTheory.ReedSolomonCodes
+import ArkLib.Data.CodingTheory.ProximityBound
+import ArkLib.Data.CodingTheory.RelativeHammingDistance
+import ArkLib.Data.CodingTheory.FieldReedSolomon
 
 import Mathlib.Probability.ProbabilityMassFunction.Basic
 import Mathlib.Probability.Distributions.Uniform
 
+open Finset ReedSolomon
+open scoped BigOperators NNReal
 
 namespace Combine
+variable {n : ℕ}
 
 /-! Section 4.5 in https://eprint.iacr.org/2024/390.pdf -/
 
 /-- Geometric series formula in a field, for a unit `r : Fˣ`. -/
-lemma geometric_sum_units {F : Type*} [Field F] [DecidableEq F] (r :  Units F) (a : ℕ) :
+lemma geometric_sum_units
+{F : Type*} [Field F] [DecidableEq F] {r :  Units F} {a : ℕ} :
   ∑ j : Fin (a + 1), (r ^ (j:ℕ) : F) =
     if r = 1 then (a + 1 : F)
     else (1 - r ^ (a + 1)) / (1 - r) := by sorry
 
 /-- Coefficients r_i as used in the definition of Combine(),
-r_0 := 1, r_i := r^{i + sum_{j < i}(d* - d_j)}
-for i > 0  (We range 0...m-1, not 1...m as in STIR)-/
-def ri {F : Type*} [Field F] {m : ℕ} (dstar : ℕ) (degs : Fin m → ℕ) (r : F) (i : Fin m) : F :=
-  if i.val = 0 then (1:F)
-  else r^(i.val + (Finset.univ.filter (· < i)).sum fun j => dstar - degs j)
+r_i := r^{i + sum_{j < i}(d* - d_j)}, note that r_0 = 1 holds in the generic form as well.
+(We iterate over 0...m-1, instead of 1...m as in STIR)-/
+def ri {F : Type*} [Semiring F] {m : ℕ} (dstar : ℕ) (degs : Fin m → ℕ) (r : F) (i : Fin m) : F :=
+r^(i.val + ∑ j < i, (dstar - degs j))
 
 /-- Combine(d*, r, (f_0, d_0), …, (f_{m-1}, d_{m-1}))(x)
 := sum_{i=0}^{m-1} r_i * f_i(x) * ( sum_{l=0}^{d* - d_i -1} (r·x)^l ) -/
 def combineInterm
-  {F : Type*} [Field F]
-  (L : Finset F)
-  (m : ℕ)
-  (dstar : ℕ)
-  (r : F)
-  (fs : Fin m → L → F)
-  (degs : Fin m → ℕ)
-  (hdegs : ∀ i, degs i ≤ dstar) : L → F :=
+  {F : Type*} [Semiring F] {m : ℕ} {ι : Type*} [Fintype ι] {domain : ι ↪ F} (dstar : ℕ) (r : F)
+  (fs : Fin m → ι → F) (degs : Fin m → ℕ)
+  (hdegs : ∀ i, degs i ≤ dstar) : ι → F :=
     fun x =>
-      (Finset.univ : Finset (Fin m)).sum
-      fun i =>
-        let di := degs i
-        let geom := (Finset.range (dstar - di + 1)).sum fun l => (r * x.val)^l
-        (ri dstar degs r i) * fs i x * geom
+        ∑ i, (ri dstar degs r i) * fs i x * (∑ l < (dstar - degs i + 1),(r * (domain x))^l)
+
+def conditionalExp
+  {F : Type*} [Field F] [DecidableEq F] {ι : Type*} [Fintype ι] (domain : ι ↪ F)
+  (dstar : ℕ) (degree : ℕ) (r : F) : ι → F :=
+  fun x =>
+    let q := r * (domain x)
+    if q = 1 then (dstar - degree + 1 : F)
+    else (1 - q^(dstar - degree + 1)) / (1 - q)
+
 
 /-- Combine(d*, r, (f_0, d_0), …, (f_{m-1}, d_{m-1}))(x) :=
       sum_{i=1}^m r_i * f_i(x) * ( (1 - (x*r)^(d* - d_i + 1)) / (1 - x*r) )    for x*r != 1,
       sum_{i=1}^m r_i * f_i(x) * ( d* - d_i + 1 )                              for x*r == 1. -/
 def combineFinal
-  {F : Type*} [Field F] [DecidableEq F]
-  {L : Finset F}
-  {m : ℕ}
-  (dstar : ℕ)
-  (r : F)
-  (fs : Fin m → L → F)
-  (degs : Fin m → ℕ)
-  (hdegs : ∀ i, degs i ≤ dstar) : L → F :=
+  {F : Type*} [Field F] [DecidableEq F] {m : ℕ} {ι : Type*} [Fintype ι] (domain : ι ↪ F)
+  (dstar : ℕ) (r : F) (fs : Fin m → ι → F)
+  (degs : Fin m → ℕ) (hdegs : ∀ i, degs i ≤ dstar) : ι → F :=
     fun x =>
-      let q := r * x.val
-      (Finset.univ : Finset (Fin m)).sum
-      fun i =>
-        let di := degs i
-        let geom := if q = 1 then (dstar - di + 1 : F)
-                    else (1 - q^(dstar - di + 1)) / (1 - q)
-        (ri dstar degs r i) * fs i x * geom
+       ∑ i, (ri dstar degs r i) * fs i x * conditionalExp domain dstar (degs i) r x
 
 /-- DegCor(d*, r, f, d)(x) := f(x) * ( sum_{l=0}^{d* - d} (r * x)^l ) -/
 def degreeCorrInterm
-{F : Type*} [Field F]
-(L : Finset F)
-(dstar : ℕ)
-(r : F)
-(f : L → F)
-(d : ℕ)
-(hd : d ≤ dstar) : L → F :=
+{F : Type*} [Semiring F] {ι : Type*} [Fintype ι] {domain : ι ↪ F}
+(dstar degree : ℕ) (r : F) (f : ι → F) (hd : degree ≤ dstar) : ι → F :=
   fun x =>
-    let geom := (Finset.range (dstar - d + 1)).sum fun l => (r * x.val) ^ l
+    let geom := ∑ l < (dstar - degree + 1), (r * (domain x)) ^ l
     f x * geom
 
 /-- DegCor(d*, r, f, d)(x) :=
       f(x) * ( (1 - (x*r)^(d* - d + 1)) / (1 - x*r) )    for x*r != 1,
       f(x) * ( d* - d + 1 )                              for x*r = 1. -/
 def degreeCorrFinal
-{F : Type*} [Field F] [DecidableEq F]
-(L : Finset F)
-(dstar : ℕ)
-(r : F)
-(f : L → F)
-(d : ℕ)
-(hd : d ≤ dstar) : L → F :=
+{F : Type*} [Field F] [DecidableEq F] {ι : Type*} [Fintype ι] {domain : ι ↪ F}
+(dstar degree : ℕ) (r : F) (f : ι → F) (hd : degree ≤ dstar) : ι → F :=
   fun x =>
-    let q := r * x.val
-    let exp := dstar - d + 1
-    let geom := if q = 1 then (dstar - d + 1 : F)
-                else (1 - q ^ exp) / (1 - q)
-    f x * geom
+    f x * conditionalExp domain dstar degree r x
 
 /-- δ ∈ (0, min {1 − B⋆(ρ), 1 − ρ − 1/|L|}) -/
  noncomputable def combineRange
   {F : Type*} [Field F] [Fintype F] [DecidableEq F]
-  {L : Finset F}
-  {d : ℕ}
-  (C : ReedSolomonCode F L d): ℝ :=
-   min (1- Bstar C.rate) (1- C.rate - 1/ Fintype.card F)
+  {ι : Type*} [Fintype ι] {degree : ℕ} {domain : ι ↪ F}
+  (C : code F ι domain degree) : ℝ :=
+   min (1- Bstar (rate C)) (1- (rate C) - 1/ Fintype.card F)
 
 /--
 If the random shift `r` causes the combined function to be far from
-the degree-`d⋆` RS code with probability exceeding `err*`,
+the degree `d⋆` RS code with probability exceeding `err*`,
 then there is a large subset `S ⊆ L` on which each `fᵢ`
-agrees with a degree-`dᵢ` Reed–Solomon codeword. -/
+agrees with a degree `dᵢ` Reed–Solomon codeword. -/
 lemma combine
-  {F : Type*} [Field F] [Fintype F] [DecidableEq F]
-  {L : Finset F}
-  {dstar : ℕ}
-  {m : ℕ}
-  (Cstar : ReedSolomonCode F L dstar)
-  (fs   : Fin m → L → F)
-  (degs : Fin m → ℕ)
-  (hdegs : ∀ i, degs i ≤ dstar)
-  (δ    : {δ // 0 < δ ∧ δ < combineRange Cstar })
-  (Ci : (i: Fin m) → ReedSolomonCode F L (degs i))
-  (hProb : (PMF.uniformOfFintype F).toOuterMeasure { r |
-          fractionalHammingDistSet
-            (combineFinal dstar r fs degs hdegs)
-            (Cstar.code)
-            (Cstar.nonempty)
-          ≤ δ.val} > err' F dstar Cstar.rate δ
-          (m * (dstar + 1) - ((Finset.univ : Finset (Fin m)).sum degs))) :
-    ∃ S : Finset F,
+  {F : Type*} [Field F] [Fintype F] [DecidableEq F] {dstar m : ℕ}
+  {ι : Type*} [Fintype ι] {domain : ι ↪ F} {L : Finset ι}
+  {Cstar : code F ι domain dstar} (fs : Fin m → ι → F)
+  (degs : Fin m → ℕ) (hdegs : ∀ i, degs i ≤ dstar)
+  (δ : ℚ) (hδPos : δ > 0) (hδLt : δ < combineRange Cstar)
+  (hProb : (PMF.uniformOfFintype F).toOuterMeasure
+              { r | δᵣ((combineFinal domain dstar r fs degs hdegs), Cstar) ≤ δ} >
+                err' F dstar (rate Cstar) δ (m * (dstar + 1) - ∑ i, degs i)) :
+    ∃ S : Finset ι,
       S ⊆ L ∧
-      S.card ≥ (1 - δ.val) * L.card ∧
-      ∀ i : Fin m, ∃ u : (L → F),
-      u ∈ (Ci i).code ∧
-      ∀ x : L, x.val ∈ S → fs i x = u x := by sorry
+      S.card ≥ (1 - δ) * L.card ∧
+      ∀ i : Fin m, ∃ u : (ι → F),
+      u ∈ (code F ι domain (degs i)) ∧
+      ∀ x : ι, x ∈ S → fs i x = u x := by sorry
 
 end Combine
