4219705e38c5685ac91872401d0c6042772fb22d
dpoulami <poulami.kgp.cse@gmail.com>
2025-05-20 13:06:00 +0200

    stir updates

--- ArkLib/ProofSystem/Stir/OutOfDomSmpl.lean
+++ ArkLib/ProofSystem/Stir/OutOfDomSmpl.lean
@@ -16,9 +16,10 @@ import Mathlib.Data.Fintype.Basic
 open ReedSolomon ListDecodable Finset
 namespace OutOfDomSmpl
 variable {F : Type*} [Field F] [Fintype F] [DecidableEq F]
-         {ι : Finset F} {domain : ι ↪ F} {degree : ℕ}
+         {ι : Finset F}
 
 /-! Section 4.3 in https://eprint.iacr.org/2024/390.pdf -/
+def toCode (LC : LinearCode ι F) : Code ι F := LC
 
 def domainComplement (ι : Finset F) : Finset F :=
   Finset.univ \ ι
@@ -26,8 +27,8 @@ def domainComplement (ι : Finset F) : Finset F :=
 /-- Pr_{r₀, …, rₛ₋ ₁  ← 𝔽\L} [∃ distinct u, u′ ∈ List(f, d, δ) :
 ∀ i ∈ [0...s-1], u(r_i) = u′(r_i)] -/
 noncomputable def listDecodingCollisionProbability
-  (f : ι → F) (δ : ℝ) (s : ℕ)
-  (h_nonempty : Nonempty (domainComplement ι))  : ENNReal :=
+  (f : ι → F) (δ : ℝ) (s : ℕ) (degree : ℕ) (domain : ι ↪ F)
+  (h_nonempty : Nonempty (domainComplement ι)) : ENNReal :=
   (PMF.uniformOfFintype (Fin s → domainComplement ι)).toOuterMeasure { r |
     ∃ (u u' : code F ι domain degree),
     -- ∃ (u u' : (relHammingBall (toLinearCode C) f δ)),
@@ -40,18 +41,18 @@ noncomputable def listDecodingCollisionProbability
   }
 
 lemma out_of_dom_smpl_1
-  (δ l : ℝ) (s : ℕ) (f : ι → F) (h_nonempty : Nonempty (domainComplement ι))
-  (h_decodable : listDecodable ↑(code F ι domain degree) δ l) :
-  listDecodingCollisionProbability f δ s h_nonempty ≤
+  (δ l : ℝ) (s : ℕ) (f : ι → F) (degree : ℕ) (domain : ι ↪ F)
+  (h_decodable : listDecodable (toCode (toLinearCode ↑(code F ι domain degree))) δ l)
+  (h_nonempty : Nonempty (domainComplement ι))  :
+  listDecodingCollisionProbability f δ s degree domain h_nonempty ≤
     (ENNReal.ofReal (l * (l-1) / 2)) * ((degree - 1) / (Fintype.card F - ι.card))^s
   := by sorry
 
 lemma out_of_dom_smpl_2
-  (f : ι → F)
-  (δ l : ℝ) (s : ℕ)
-  (h_decodable : listDecodable ↑(code F ι domain degree) δ l)
+  (δ l : ℝ) (s : ℕ) (f : ι → F) (degree : ℕ) (domain : ι ↪ F)
+  (h_decodable : listDecodable (toCode (toLinearCode ↑(code F ι domain degree))) δ l)
   (h_nonempty : Nonempty (domainComplement ι)) :
-  listDecodingCollisionProbability f δ s h_nonempty ≤
+  listDecodingCollisionProbability f δ s degree domain h_nonempty ≤
     (ENNReal.ofReal (l^2 / 2)) * (degree / (Fintype.card F - ι.card))^s
   := by sorry
 
