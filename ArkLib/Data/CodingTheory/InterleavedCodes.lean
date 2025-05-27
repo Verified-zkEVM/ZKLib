bd84165da0ca9915e0261eb27506fbc84bc1ab46
dpoulami <poulami.kgp.cse@gmail.com>
2025-05-27 17:23:38 +0200

    minor

--- ArkLib/Data/CodingTheory/InterleavedCodes.lean
+++ ArkLib/Data/CodingTheory/InterleavedCodes.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024-2025 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Katerina Hristova, František Silváši, Least Authority
+Authors: Katerina Hristova, František Silváši
 -/
 
 import Mathlib.Algebra.Module.Submodule.Defs
