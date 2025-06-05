25dfa8d51220e08af4999168fa78b51734417437
dpoulami <poulami.kgp.cse@gmail.com>
2025-06-05 13:44:48 +0200

    refine coding theory defs

--- ArkLib/Data/CodingTheory/ConstraintReedSolomon.lean
+++ ArkLib/Data/CodingTheory/ConstraintReedSolomon.lean
@@ -8,169 +8,44 @@ import ArkLib.Data.CodingTheory.SmoothReedSolomon
 
 namespace ReedSolomon
 
-
-open ReedSolomon SmoothDomain Finset
+open Finset SmoothDomain
 
 /-- Auxiliary function to assign values to the weight polynomial variables:
     index `0` ↦ `p.eval b`, index `j+1` ↦ `b j`. -/
 private def toWeightAssignment
-  {F : Type*} [Field F]
-  {m : ℕ}
-  (p : MvPolynomial (Fin m) F)
+  {F : Type*} [Field F] {m : ℕ} (p : MvPolynomial (Fin m) F)
   (b : Fin m → Fin 2) : Fin (m+1) → F :=
     let b' : Fin m → F := fun i => ↑(b i : ℕ)
     Fin.cases (MvPolynomial.eval b' p)
               (fun i => ↑(b i : ℕ))
 
-
-/-- constraint is true, if ∑_{b ∈ {0,1}^m} w(f(b),b) = σ for given
-    m-variate polynomial `f`and `(m+1)`-variate polynomial `w`-/
+/-- constraint is true, if ∑ {b ∈ {0,1}^m} w(f(b),b) = σ for given
+    m-variate polynomial `f` and `(m+1)`-variate polynomial `w`-/
 def weightConstraint
-  {F : Type*} [Field F] [DecidableEq F]
-  {m : ℕ}
-  (f : MvPolynomial (Fin m) F)
-  (w : MvPolynomial (Fin (m+1)) F)
-  (σ : F): Prop :=
+  {F : Type*} [Field F] [DecidableEq F] {m : ℕ} (f : MvPolynomial (Fin m) F)
+  (w : MvPolynomial (Fin (m+1)) F) (σ : F): Prop :=
     ∑ b : Fin m → Fin 2 , w.eval (toWeightAssignment f b) = σ
 
 /-- Constraint Reed Solomon codes are smooth codes who's decoded m-variate
-    polynomial satisfies the weight constraint for given `w` and `σ`.
-
-    The following definition returns a subset of smooth ReedSolomon codes
-    satisfying the weight constraint. This version can be used when we require
-    the decoded multivariate polynomial from an underlying smooth codeword.-/
-
-def constraintCodesetSC
-  (F : Type*) [Field F] [DecidableEq F]
-  (ι : Type*) [Fintype ι] [DecidableEq ι]
-  (domain : ι ↪ F)
-  [Smooth domain]
-  (m : ℕ)
-  (w : MvPolynomial (Fin (m+1)) F)
-  (σ : F):=
-    {sc : smoothCode F ι domain m | weightConstraint  (mVdecode sc) w σ}
-
-/--Alternatively, the following definition returns a Set (ι -> F). -/
-
+    polynomial satisfies the weight constraint for given `w` and `σ`.-/
 def constraintCode
   (F : Type*) [Field F] [DecidableEq F]
   (ι : Type*) [Fintype ι] [DecidableEq ι]
-  (domain : ι ↪ F)
-  (m : ℕ) [Smooth domain]
-  (w : MvPolynomial (Fin (m+1)) F)
-  (σ : F)
-  : Set (ι → F) :=
+  (domain : ι ↪ F) [Smooth domain] (m : ℕ)
+  (w : MvPolynomial (Fin (m+1)) F) (σ : F) : Set (ι → F) :=
     { f | ∃ (h : f ∈ smoothCode F ι domain m),
       weightConstraint (mVdecode (⟨f, h⟩ : smoothCode F ι domain m)) w σ }
 
-
-namespace ConstraintReedSolomon
-
-variable  {F : Type*} [Field F] [DecidableEq F]
-          {ι : Finset F} [DecidableEq ι]
-          {domain : ι ↪ F}
-          [Smooth domain]
-          {m : ℕ}
-          {w : MvPolynomial (Fin (m+1)) F}
-          {σ : F}
-
-/-- Forget the weight constrain. -/
-noncomputable def toSmoothCode
-  (cc : constraintCodesetSC F ι domain m w σ) : smoothCode F ι domain m :=
-    cc.val
-
-section --Test
-
-variable  (F : Type*) [Field F] [DecidableEq F]
-          (ι : Finset F) [DecidableEq ι]
-          (domain : ι ↪ F)
-          [Smooth domain]
-          (m : ℕ)
-          (w : MvPolynomial (Fin (m+1)) F)
-          (σ : F)
-          (cc : constraintCodesetSC F ι domain m w σ)
-
-
-#check constraintCodesetSC F ι domain m w σ
-#check cc
-#check mVdecode (toSmoothCode cc)
-
-end
-
-end ConstraintReedSolomon
-
-
 /-- Multi-constraint Reed Solomon codes are smooth codes who's decoded m-variate
     polynomial satisfies the `t` weight constraints for given `w₀,...,wₜ₋₁` and
-    `σ₀,...,σₜ₋₁`.
-
-    The following definition returns a subset of smooth ReedSolomon codes
-    satisfying the weight constraint. This version can be used when we require
-    the decoded multivariate polynomial from an underlying smooth codeword.-/
-def multiConstraintCodesetSC
-  (F : Type*) [Field F] [DecidableEq F]
-  (ι : Type*) [Fintype ι] [DecidableEq ι]
-  (domain : ι ↪ F)
-  [Smooth domain]
-  (m : ℕ)
-  (t : ℕ)
-  (w : Fin t → MvPolynomial (Fin (m+1)) F)
-  (σ : Fin t → F) :
-  Set (smoothCode F ι domain m) :=
-    { sc | ∀ i : Fin t, weightConstraint (mVdecode sc) (w i) (σ i) }
-
-/--Alternatively, the following definition returns a Set (ι -> F). -/
+    `σ₀,...,σₜ₋₁`.-/
 def multiConstraintCode
   (F : Type*) [Field F] [DecidableEq F]
   (ι : Type*) [Fintype ι] [DecidableEq ι]
-  (domain : ι ↪ F)
-  [Smooth domain]
-  (m : ℕ)
-  (t : ℕ)
-  (w : Fin t → MvPolynomial (Fin (m+1)) F)
-  (σ : Fin t → F) :
-  Set (ι → F) :=
+  (domain : ι ↪ F) [Smooth domain] (m t : ℕ)
+  (w : Fin t → MvPolynomial (Fin (m+1)) F) (σ : Fin t → F) : Set (ι → F) :=
     { f |
       ∃ (h : f ∈ smoothCode F ι domain m),
         ∀ i : Fin t, weightConstraint (mVdecode (⟨f, h⟩ : smoothCode F ι domain m)) (w i) (σ i)}
-
-
-
-namespace MultiConstraintReedSolomon
-
-variable  {F : Type*} [Field F] [DecidableEq F]
-          {ι : Type*} [Fintype ι] [DecidableEq ι]
-          {domain : ι ↪ F}
-          [Smooth domain]
-          {m : ℕ}
-          {t : ℕ}
-          {w : Fin t → MvPolynomial (Fin (m+1)) F}
-          {σ : Fin t → F}
-
-/-- Forget all weight constraints. -/
-noncomputable def toSmoothCode
-  (cc : multiConstraintCodesetSC F ι domain m t w σ) : smoothCode F ι domain m :=
-    cc.val
-
-section
-
-variable  (F : Type*) [Field F] [DecidableEq F]
-          {ι : Type*} [Fintype ι] [DecidableEq ι]
-          (domain : ι ↪ F)
-          [Smooth domain]
-          (m : ℕ)
-          (t : ℕ)
-          (w : Fin t → MvPolynomial (Fin (m+1)) F)
-          (σ : Fin t → F)
-          (mcc : multiConstraintCodesetSC F ι domain m t w σ)
-
-#check multiConstraintCode F ι domain m t w σ
-#check mcc
-#check mVdecode (toSmoothCode mcc)
-
-end
-
-end MultiConstraintReedSolomon
-
 
 end ReedSolomon
--- ArkLib/Data/CodingTheory/SmoothReedSolomon.lean
+++ ArkLib/Data/CodingTheory/SmoothReedSolomon.lean
@@ -3,13 +3,34 @@ Copyright (c) 2025 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Least Authority
 -/
+
 import ArkLib.Data.CodingTheory.FieldReedSolomon
-import ArkLib.Data.CodingTheory.SmoothDomainGeneric
 import ArkLib.Data.MvPolynomial.LinearMvExtension
 
+namespace SmoothDomain
+variable {F: Type*} [Semiring F] [DecidableEq F]
+         {ι : Type*} [Fintype ι]
+
+/-- A domain `ι ↪ F` is `smooth`, if `ι ⊆ F`, `|ι| = 2^k` for some `k` and
+    there exists a subgroup `H` in the group of units `Rˣ`
+    and an invertible element `a ∈ R` such that `ι = a • H` -/
+class Smooth
+  [DecidableEq ι]
+  (domain : ι ↪ F) where
+    H           : Subgroup (Units F)
+    a           : Units F
+    h_coset     : Finset.image domain Finset.univ
+                  = (fun h : Units F => (a : F) * (h : F)) '' (H : Set (Units F))
+    h_card_pow2 : ∃ k : ℕ, Fintype.card ι = 2 ^ k
+
+end SmoothDomain
 namespace ReedSolomon
 
-open ReedSolomon SmoothDomain LinearMvExtension
+open LinearMvExtension ReedSolomon SmoothDomain
+variable  {F : Type*} [Field F] [DecidableEq F]
+          {ι : Type*} [Fintype ι] [DecidableEq ι]
+          {domain : ι ↪ F} [Smooth domain]
+          {m : ℕ}
 
 /-- Smooth Reed Solomon Codes are Reed Solomon Codes defined over Smooth Domains, such that
     their decoded univariate polynomials are of degree < 2ᵐ for some m ∈ ℕ. -/
@@ -20,34 +41,9 @@ def smoothCode
   (m : ℕ): Submodule F (ι → F) :=
     code F ι domain (2^m)
 
-section SmoothRSC
-
-variable  {F : Type*} [Field F] [DecidableEq F]
-          {ι : Type*} [Fintype ι] [DecidableEq ι]
-          {domain : ι ↪ F} [Smooth domain]
-          {m : ℕ}
-
 /-- The linear map that maps Smooth Reed Solomon Code words
     to their decoded degree wise linear `m`-variate polynomial  -/
 noncomputable def mVdecode : (smoothCode F ι domain m) →ₗ[F] MvPolynomial (Fin m) F :=
   linearMvExtension.comp decodeLT
-
-section -- Test
-
-variable (F : Type*) [Field F] [DecidableEq F]
-         (ι : Type*) [Fintype ι] [DecidableEq ι]
-         (domain : ι ↪ F) [Smooth domain]
-         (deg : ℕ)
-         (m : ℕ)
-         (c : code F ι domain deg)
-         (sc : smoothCode F ι domain m)
-
-#check c
-#check sc
-#check mVdecode sc
-
-end
-
-end SmoothRSC
 
 end ReedSolomon
