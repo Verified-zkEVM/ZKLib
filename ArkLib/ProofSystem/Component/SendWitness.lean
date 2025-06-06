a0835c09cc31be04ffb5254b22e65bcfbb9bf65b
Quang Dao <qvd@andrew.cmu.edu>
2025-06-06 10:12:37 +0700

    more on `SendWitness`

--- ArkLib/ProofSystem/Component/SendWitness.lean
+++ ArkLib/ProofSystem/Component/SendWitness.lean
@@ -4,71 +4,98 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Quang Dao
 -/
 import ArkLib.OracleReduction.Security.Basic
+import Mathlib.Data.FinEnum
 
 /-!
 # Simple Oracle Reduction - SendWitness
 
-- The prover sends an oracle of type `WitEquiv` (supposed to be the witness up to equivalence) to
-     the verifier.
-   - The verifier does nothing.
-   - The output data has the same `Statement`, have output `OStatement` be the previous `OStatement`
-     plus an oracle for `EncWit`, and have no witness.
-   - The new relation is `rel` but replacing `Witness` with `EncWit`.
+This file contains the (oracle) reduction for the trivial one-message protocol where the prover
+sends the (entire) witness to the verifier. There are two variants:
 
-This is usually used as the first step of an oracle reduction, to replace the witness with an
-oracle statement.
+1. For oracle reduction: the witness is a finitely indexed type, and sent as a series of oracle
+   statements to the verifier.
+
+2. For reduction: the witness is a type, and sent as a statement to the verifier.
 -/
 
-open OracleSpec OracleComp OracleQuery
+open OracleSpec OracleComp OracleQuery ProtocolSpec
 
 namespace SendWitness
 
-variable {ι : Type} (oSpec : OracleSpec ι) (Statement Witness : Type)
-  {ιₛᵢ : Type} (OStatement : ιₛᵢ → Type) [∀ i, OracleInterface (OStatement i)]
-  (WitEquiv : Type) [OracleInterface WitEquiv] (equiv : Witness ≃ WitEquiv)
+variable {ι : Type} (oSpec : OracleSpec ι) (Statement : Type)
 
-@[reducible]
-def pSpec : ProtocolSpec 1 := ![(.P_to_V, WitEquiv)]
+/-!
+  First, the reduction version (no oracle statements)
+-/
 
--- TODO: figure out why `OracleInterface` & `VCVCompatible` instances cannot be automatically
--- synthesized
-instance : ∀ i, OracleInterface ((pSpec WitEquiv).Message i)
-  | ⟨0, _⟩ => by dsimp; infer_instance
-instance : ∀ i, VCVCompatible ((pSpec WitEquiv).Challenge i) | ⟨0, h⟩ => nomatch h
+section Reduction
 
-def prover : OracleProver (pSpec WitEquiv) oSpec Statement Witness Statement Unit
-    OStatement (OStatement ⊕ᵥ (fun _ : Unit => WitEquiv)) where
+variable (Witness : Type)
+
+@[reducible, simp]
+def pSpec : ProtocolSpec 1 := ![(.P_to_V, Witness)]
+
+instance : ∀ i, VCVCompatible ((pSpec Witness).Challenge i) | ⟨0, h⟩ => nomatch h
+
+@[inline, specialize]
+def prover : Prover (pSpec Witness) oSpec Statement Witness (Statement × Witness) Unit where
   PrvState
-  | 0 => Statement × (∀ i, OStatement i) × Witness
-  | 1 => Statement × (∀ i, OStatement i) × WitEquiv
-  input := fun ⟨stmt, oStmt⟩ wit => ⟨stmt, oStmt, wit⟩
-  sendMessage | ⟨0, _⟩ => fun ⟨stmt, oStmt, wit⟩ => pure (equiv wit, ⟨stmt, oStmt, equiv wit⟩)
+  | 0 => Statement × Witness
+  | 1 => Statement × Witness
+  input := fun stmt wit => ⟨stmt, wit⟩
+  sendMessage | ⟨0, _⟩ => fun ⟨stmt, wit⟩ => pure (wit, ⟨stmt, wit⟩)
   receiveChallenge | ⟨0, h⟩ => nomatch h
-  output := fun ⟨stmt, oStmt, encWit⟩ => (⟨stmt, (Sum.rec oStmt (fun _ => encWit))⟩, ())
+  output := fun ⟨stmt, wit⟩ => (⟨stmt, wit⟩, ())
 
-def verifier : OracleVerifier (pSpec WitEquiv) oSpec Statement Statement
-    OStatement (OStatement ⊕ᵥ (fun _ : Unit => WitEquiv)) where
-  verify := fun stmt _ => pure stmt
-  embed := by simp [pSpec, ProtocolSpec.MessageIdx]; sorry
-  hEq := sorry
+@[inline, specialize]
+def verifier : Verifier (pSpec Witness) oSpec Statement (Statement × Witness) where
+  verify := fun stmt transcript => pure ⟨stmt, transcript 0⟩
 
-def oracleReduction : OracleReduction (pSpec WitEquiv) oSpec Statement Witness Statement Unit
-    OStatement (OStatement ⊕ᵥ (fun _ : Unit => WitEquiv)) where
-  prover := prover oSpec Statement Witness OStatement WitEquiv equiv
-  verifier := verifier oSpec Statement OStatement WitEquiv
+@[inline, specialize]
+def reduction : Reduction (pSpec Witness) oSpec Statement Witness (Statement × Witness) Unit where
+  prover := prover oSpec Statement Witness
+  verifier := verifier oSpec Statement Witness
 
-variable (relIn : Statement × (∀ i, OStatement i) → Witness → Prop)
+variable {Statement} {Witness} [oSpec.FiniteRange] (relIn : Statement → Witness → Prop)
 
-def toRelOut : Statement × (∀ i, (OStatement ⊕ᵥ (fun _ : Unit => WitEquiv)) i) → Unit → Prop :=
-  fun ⟨stmt, oStmt⟩ _ => relIn ⟨stmt, fun i => oStmt (Sum.inl i)⟩ (equiv.invFun <| oStmt (.inr ()))
+@[reducible, simp]
+def toRelOut : Statement × Witness → Unit → Prop :=
+  fun ⟨stmt, wit⟩ _ => relIn stmt wit
 
-variable [oSpec.FiniteRange]
+/-- The `SendWitness` reduction satisfies perfect completeness. -/
+@[simp]
+theorem reduction_completeness :
+    (reduction oSpec Statement Witness).perfectCompleteness relIn (toRelOut relIn) := by
+  simp [Reduction.run, Prover.run, Prover.runToRound, Prover.processRound, Verifier.run,
+    reduction, prover, verifier]
+  aesop
 
-theorem completeness : OracleReduction.perfectCompleteness
-    relIn
-    (toRelOut Statement Witness OStatement WitEquiv equiv relIn)
-    (oracleReduction oSpec Statement Witness OStatement WitEquiv equiv) := by
-  sorry
-  -- simp [OracleReduction.perfectCompleteness]
+#check MonadLiftT
+
+theorem reduction_rbr_knowledge_soundness : True := sorry
+
+end Reduction
+
+/-!
+  Now, the oracle reduction version
+-/
+
+section OracleReduction
+
+variable {ιₛ : Type} (OStatement : ιₛ → Type) [∀ i, OracleInterface (OStatement i)]
+  {ιw : Type} [FinEnum ιw] (Witness : ιw → Type) [∀ i, OracleInterface (Witness i)]
+
+@[reducible, simp]
+def oraclePSpec : ProtocolSpec (FinEnum.card ιw) :=
+  fun i => (.P_to_V, Witness (FinEnum.equiv.symm i))
+
+instance : IsEmpty (oraclePSpec Witness).ChallengeIdx where
+  false := by aesop
+instance : ∀ i, OracleInterface ((oraclePSpec Witness).Message i) :=
+  fun _ => inferInstance
+instance : ∀ i, VCVCompatible ((oraclePSpec Witness).Challenge i) :=
+  fun _ => by aesop
+
+end OracleReduction
 
 end SendWitness
