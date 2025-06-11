606ae940f423862bb3610c59244188cedec1310b
dpoulami <poulami.kgp.cse@gmail.com>
2025-06-11 22:22:37 +0200

    resolved import conflicts, adjusted type, audited code

--- ArkLib/Data/Probability/Notation.lean
+++ ArkLib/Data/Probability/Notation.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Quang Dao, František Silváši, Least Authority
+Authors: Quang Dao, František Silváši
 -/
 
 import Mathlib.Probability.Notation
