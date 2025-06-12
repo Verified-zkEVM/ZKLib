af65ef1bb491fff0b967f2d77fdb5d5df899ffa8
dpoulami <poulami.kgp.cse@gmail.com>
2025-06-12 11:08:16 +0200

    added proximity gap thm in stir as well

--- ArkLib/ProofSystem/Stir.lean
+++ ArkLib/ProofSystem/Stir.lean
@@ -3,4 +3,5 @@ import ArkLib.ProofSystem.Stir.Folding
 import ArkLib.ProofSystem.Stir.MainThm
 import ArkLib.ProofSystem.Stir.OutOfDomSmpl
 import ArkLib.ProofSystem.Stir.ProximityBound
+import ArkLib.ProofSystem.Stir.ProximityGap
 import ArkLib.ProofSystem.Stir.Quotienting
